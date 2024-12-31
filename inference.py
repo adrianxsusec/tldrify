ae1c991bee703f64e86197822ab0b450f08d9dc2
Rishi Vijayvargiya <rishivijayv@gmail.com>
2024-12-31 11:37:11 -0500

    fix: using different fs name and datetime to get time in utc

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import hopsworks
 import sys
 import pandas as pd
 
-from datetime import date, timedelta
+from datetime import datetime, timezone, timedelta
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 date_format = '%Y-%m-%d'
@@ -16,8 +16,8 @@ def fetch_todays_articles(fs):
 
     news_articles_df = news_articles_hopsworks.read()
     news_articles_df['pubdate'] = pd.to_datetime(news_articles_df['pubdate']).dt.strftime(date_format)
-    today = date.today().strftime(date_format)
-    yesterday = (date.today() - timedelta(days=1)).strftime(date_format)
+    today = datetime.now(timezone.utc).strftime(date_format)
+    yesterday = (datetime.now(timezone.utc) - timedelta(days=1)).strftime(date_format)
 
     mask = news_articles_df['pubdate'] == yesterday
     return news_articles_df[mask]
@@ -42,7 +42,7 @@ def summarize_and_push_articles_to_fs(fs, articles, model, tokenizer):
         print(f"\n{'=' * 100}")
 
     # news_articles_hopsworks = fs.get_or_create_feature_group(
-    #     name="news_articles_raw",
+    #     name="news_articles_summarized",
     #     version=1,
     #     primary_key=['article_id'],
     #     description="Summarized articles from news_articles_raw"
